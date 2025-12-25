9dd6488ff7061e3165a4a5c06d82cb40b2cc77f6
haseeb_khan <haseebkhanvolt@gmail.com>
2025-12-25 19:06:27 +0500

    fix: UI fixes in chat screen

--- app/(app)/chats/[id].tsx
+++ app/(app)/chats/[id].tsx
@@ -8,10 +8,11 @@ import { supabase } from "@/lib/supabase";
 import { Ionicons } from "@expo/vector-icons";
 import { useQueryClient } from "@tanstack/react-query";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import React, { useCallback, useEffect, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { Image, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { GiftedChat, IMessage, Send } from "react-native-gifted-chat";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Composer, GiftedChat, IMessage, InputToolbar, Send } from "react-native-gifted-chat";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+
 
 export default function ChatDetailScreen() {
     const router = useRouter();
@@ -19,7 +20,13 @@ export default function ChatDetailScreen() {
     const { user } = useSession();
     const { id: roomId, otherUserId, otherUserName, otherUserAvatar } = useLocalSearchParams();
     const currentUserId = user?.id;
+    const insets = useSafeAreaInsets();
+    const [keyboardHeight, setKeyboardHeight] = useState(0);
 
+
+    const tabBarHeight = 50;
+    const keyboardTopToolbarHeight = Platform.select({ ios: 44, default: 0 })
+    const keyboardVerticalOffset = insets.top + insets.bottom + 50;
     const {
         data,
         fetchNextPage,
@@ -72,27 +79,48 @@ export default function ChatDetailScreen() {
 
     const markAsRead = async () => {
         const { error } = await supabase
-          .from('messages')
-          .update({ is_read: true })
-          .eq('room_id', roomId)
-          .neq('sender_id', user?.id)
-          .eq('is_read', false);
-          
+            .from('messages')
+            .update({ is_read: true })
+            .eq('room_id', roomId)
+            .neq('sender_id', user?.id)
+            .eq('is_read', false);
+
         if (error) console.error("Error marking read:", error);
-      };
-      
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         if (roomId && user?.id) {
-          markAsRead();
+            markAsRead();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [roomId, user?.id]);
+    }, [roomId, user?.id]);
 
     const onSend = useCallback((newMessages: IMessage[] = []) => {
         if (newMessages.length > 0) {
             sendMessage(newMessages[0].text);
         }
     }, [sendMessage]);
+
+    // useEffect(() => {
+    //     const keyboardDidShowListener = Keyboard.addListener(
+    //         Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow',
+    //         (e) => {
+    //             setKeyboardHeight(e.endCoordinates.height);
+    //         }
+    //     );
+    //     const keyboardDidHideListener = Keyboard.addListener(
+    //         Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide',
+    //         () => {
+    //             setKeyboardHeight(0);
+    //         }
+    //     );
+
+    //     return () => {
+    //         keyboardDidShowListener.remove();
+    //         keyboardDidHideListener.remove();
+    //     };
+    // }, []);
+
 
     return (
         <SafeAreaView style={styles.container} edges={["top"]}>
@@ -129,16 +157,30 @@ export default function ChatDetailScreen() {
                 }}
                 renderBubble={(props) => <ChatBubble {...props} />}
                 renderSend={(props) => (
-                    <Send {...props} containerStyle={styles.sendContainer}>
+                    <Send {...props} containerStyle={styles.sendContainer} isSendButtonAlwaysVisible>
                         <View style={styles.sendButton}>
                             <Ionicons name="send" size={20} color={Colors.white} />
                         </View>
                     </Send>
                 )}
                 renderChatEmpty={() => <EmptyChat otherUserName={otherUserName as string} />}
+                renderInputToolbar={(props) => (
+                    <InputToolbar {...props}
+                        containerStyle={styles.inputToolbar}
+                    />
+                )}
+                renderComposer={(props) => (
+                    <Composer
+                        {...props}
+                        textInputProps={{
+                            ...(props.textInputProps ?? {}),
+                            style: styles.inputText,
+                        }}
+                    />
+                )}
                 messagesContainerStyle={{ backgroundColor: Colors.background }}
             />
-            {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" style={{paddingBottom: 12}} />}
+            {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />}
         </SafeAreaView>
     );
 }
@@ -187,7 +229,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginRight: 10,
-        marginBottom: 5,
+        marginBottom: 10,
+        marginTop: 10,
     },
     sendButton: {
         width: 40,
@@ -196,5 +239,24 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.primary,
         alignItems: "center",
         justifyContent: "center",
+    },
+    inputToolbar: {
+        backgroundColor: Colors.white,
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        paddingBottom: 16,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    inputText: {
+        backgroundColor: Colors.greyLight,
+        color: Colors.text,
+        borderRadius: 20,
+        paddingVertical: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginLeft: 0,
+        marginBottom: 8,
+        marginRight: 16,
     },
 });
